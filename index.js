ac328f13be93648e62d89e5a212a7271d2c757c7
Jamar Graves <jamargraves07@gmail.com>
2023-08-21 15:15:03 -0400

    Completed lab

---
+++ index.js
@@ -0,0 +1,29 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+ //completes the 1st test 
+function logShout(string) {
+    console.log(string.toUpperCase())
+}
+ // completes the 2nd test
+function logWhisper(string){ 
+    console.log(string.toLowerCase())
+}
+ // completes the 3rd test
+function sayHiToHeadphonedRoommate (string){
+
+if (string === string.toLowerCase()) { 
+return "I can't hear you!"
+}
+if (string === string.toUpperCase()) { 
+    return "YES INDEED!"
+
+}
+if (string === "Let's have dinner together!") { 
+    return "I would love to!"
+}
+}
